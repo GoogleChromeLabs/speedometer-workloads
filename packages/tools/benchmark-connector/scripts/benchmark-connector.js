96f362a469a031c3c40efcd45617e60b6212a8e4
Thorsten Kober <tkober@gmail.com>
2024-05-06 12:41:58 -0400

    simplify benchmark-connector

--- packages/tools/benchmark-connector/scripts/benchmark-connector.js
+++ packages/tools/benchmark-connector/scripts/benchmark-connector.js
@@ -1,39 +1,3 @@
-/************************************************************************ 
-  Custom History Events
-
-  Augments some history events with additional events for consistency:
-
-  - window.pushstate
-  - window.replacestate
-  - window.statechange
-*************************************************************************/
-function triggerEvent(element, name, state) {
-  const event = new CustomEvent(name);
-  event.name = name;
-  event.state = state;
-  element.dispatchEvent(event);
-}
-
-const pushState = history.pushState;
-history.pushState = function (state) {
-  const fn = pushState.apply(history, arguments);
-  triggerEvent(window, "pushstate", state);
-  triggerEvent(window, "statechange", state);
-  return fn;
-};
-
-const replaceState = history.replaceState;
-history.replaceState = function (state) {
-  const fn = replaceState.apply(history, arguments);
-  triggerEvent(window, "replacestate", state);
-  triggerEvent(window, "statechange", state);
-  return fn;
-};
-
-window.addEventListener("popstate", function (event) {
-  triggerEvent(window, "statechange", event.state);
-});
-
 /************************************************************************
  * Benchmark Connector
  *
@@ -52,33 +16,6 @@ window.onmessage = async (event) => {
   // ensure we only let legit functions run...
   if (event.data.id !== appId || event.data.type !== "benchmark-connector")
     return;
-
-  const { name } = event.data;
-
-  // if a test function was received from a message, the app will attempt to run the test and report back the results.
-  const testFunction = new Function(`return ${event.data.fn}`)();
-  if (testFunction) {
-    requestAnimationFrame(() => {
-      performance.mark(`${name}-start`);
-    });
-    requestAnimationFrame(async () => {
-      await testFunction();
-      setTimeout(() => {
-        performance.mark(`${name}-end`);
-        performance.measure(`${name}`, {
-          start: `${name}-start`,
-          end: `${name}-end`,
-        });
-        const result = JSON.stringify(
-          performance.getEntriesByName(`${name}`)[0]
-        );
-        window.top.postMessage(
-          { type: "test-completed", status: "success", result },
-          "*"
-        );
-      }, 0);
-    });
-  }
 };
 
 window.top.postMessage({ type: "app-ready", status: "success", appId }, "*");
